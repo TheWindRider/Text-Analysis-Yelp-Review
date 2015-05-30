ecc215228685a47a641616bcd116b83262d1f75d
TheWindRider <ychg235711@gmail.com>
2015-05-30 13:11:04 -0700

    update

--- Unit_2/exercise_2.py
+++ Unit_2/exercise_2.py
@@ -1,3 +1,5 @@
+"""
+# univariate - toy data
 import os
 import matplotlib.pyplot as plt
 import scipy.stats as stats
@@ -13,3 +15,22 @@ plt.close()
 stats.probplot(x, dist = 'norm', plot = plt)
 plt.savefig('qq.png')
 plt.close()
+"""
+# univariate - lending
+import matplotlib.pyplot as plt
+import pandas as pd
+import scipy.stats as stats
+import os
+
+loansData = pd.read_csv('https://spark-public.s3.amazonaws.com/dataanalysis/loansData.csv')
+loansData.dropna(inplace=True)
+os.chdir('Documents/Thinkful Project/thinkful course/Unit_2')
+
+loansData.boxplot(column='Amount.Requested')
+plt.savefig('loan_box.png')
+plt.close()
+loansData.hist(column='Amount.Requested')
+plt.savefig('loan_hist.png')
+plt.close()
+stats.probplot(loansData['Amount.Requested'], dist="norm", plot=plt)
+plt.savefig('loan_qq.png')
