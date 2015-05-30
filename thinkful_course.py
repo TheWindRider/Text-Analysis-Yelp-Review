1fbffdac56f61f2e83e46f94cd3bcbaba7cf432d
TheWindRider <ychg235711@gmail.com>
2015-05-29 22:11:40 -0700

    submit another assignment

--- thinkful_course.py
+++ thinkful_course.py
@@ -66,7 +66,7 @@ with open('SQLite Data/EarthInstitute/thinkful_analysis.csv', 'w') as output_fil
     output_file.write('continent,ppl_change_2100_2010,ppl_density_2010\n')
     for continent, info in continent_wiki.iteritems():
         output_file.write(continent + ',' + str((info[1] - info[0])/float(info[0])) + ',' + str(info[0]/float(info[2])) + '\n')
-"""
+
 # SQLite 
 import sqlite3 as lite
 import pandas as pd
@@ -96,3 +96,35 @@ with con:
             else: 
                 message += '; '
 print message
+"""
+# Statistics Overview
+import pandas as pd
+from scipy import stats
+
+data = '''Region, Alcohol, Tobacco
+North, 6.47, 4.03
+Yorkshire, 6.13, 3.76
+Northeast, 6.19, 3.77
+East Midlands, 4.89, 3.34
+West Midlands, 5.63, 3.47
+East Anglia, 4.52, 2.92
+Southeast, 5.89, 3.20
+Southwest, 4.79, 2.71
+Wales, 5.27, 3.53
+Scotland, 6.08, 4.51
+Northern Ireland, 4.02, 4.56'''
+data = data.splitlines()
+data = [i.split(', ') for i in data]
+column_names = data[0]
+data_rows = data[1::]
+df = pd.DataFrame(data_rows, columns=column_names)
+df['Alcohol'] = df['Alcohol'].astype(float)
+df['Tobacco'] = df['Tobacco'].astype(float)
+
+means = [df['Alcohol'].mean(), df['Tobacco'].mean()]
+medians = [df['Alcohol'].median(), df['Tobacco'].median()]
+modes = [stats.mode(df['Alcohol'])[0][0], stats.mode(df['Tobacco'])[0][0]]
+ranges = [max(df['Alcohol']) - min(df['Alcohol']), max(df['Tobacco']) - min(df['Tobacco'])]
+variances = [df['Alcohol'].var(), df['Tobacco'].var()]
+deviations = [df['Alcohol'].std(), df['Tobacco'].std()]
+print "For the Alcohol and Tobacco dataset, means are {0}, medians are {1}, modes are {2}, ranges are {3}, variances are {4}, and standard deviations are {5}".format(means, medians, modes, ranges, variances, deviations)
