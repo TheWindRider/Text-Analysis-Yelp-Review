b29a267b0ca19da008fb51fbd4d6ac175b97b19a
TheWindRider <ychg235711@gmail.com>
2015-05-30 18:44:28 -0700

    linear model

--- Unit_2/exercise_2.py
+++ Unit_2/exercise_2.py
@@ -34,7 +34,7 @@ plt.savefig('loan_hist.png')
 plt.close()
 stats.probplot(loansData['Amount.Requested'], dist="norm", plot=plt)
 plt.savefig('loan_qq.png')
-"""
+
 # chi-square test
 from scipy import stats
 import pandas as pd
@@ -49,3 +49,31 @@ if p < 0.05:
     print "Null hypothesis rejected, not even distribution"
 else: 
     print "Cannot reject the null hypotheis on an even distribution"
+"""
+# Linear Regression
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
+
+loansData = pd.read_csv('https://spark-public.s3.amazonaws.com/dataanalysis/loansData.csv')
+loansData.dropna(inplace=True)
+
+loansData['Interest.Rate'] = loansData['Interest.Rate'].apply(lambda x: float(x.rstrip('%'))/100)
+loansData['Loan.Length'] = loansData['Loan.Length'].apply(lambda x: int(x.split(' ')[0]))
+loansData['FICO.Score'] = loansData['FICO.Range'].apply(lambda x: int(x.split('-')[0]))
+
+pd.scatter_matrix(loansData, alpha=0.05, figsize=(10,10), diagonal='hist')
+plt.show()
+
+intrate = loansData['Interest.Rate']
+loanamt = loansData['Amount.Requested']
+fico = loansData['FICO.Score']
+
+dep_var = np.matrix(intrate).transpose()
+exp_1 = np.matrix(fico).transpose()
+exp_2 = np.matrix(loanamt).transpose()
+exp_both = np.column_stack([exp_1, exp_2])
+exp_var = sm.add_constant(exp_both)
+model = sm.OLS(dep_var, exp_var)
+print model.fit().summary()
