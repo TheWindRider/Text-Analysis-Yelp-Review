56a9eb18d2cca016fbc86c10aee58aa82af3911a
TheWindRider <ychg235711@gmail.com>
2015-08-02 19:52:31 -0700

    subset with keyword

--- capstone/extract_review.py
+++ capstone/extract_review.py
@@ -4,43 +4,28 @@ import imp
 import math
 import pandas
 from textblob import TextBlob
-from collections import defaultdict
+
 """ POS tag reference
 https://www.ling.upenn.edu/courses/Fall_2003/ling001/penn_treebank_pos.html
 """
 DROP_TAG = set(['CC', 'CD', 'DT', 'EX', 'IN', 'MD', 'NNP', 'NNPS', 
                 'POS', 'PRP', 'PRP$', 'SYM', 'TO', 'UH', 'WDT', 'WP', 'WRB'])
 DROP_WORD = set(['also', 'anyone', 'asked', 'been', 'did', 'even', 'ever', 'get', 'good', 'got', 
-                 'have', 'had', 'here', 'just', 'make', 'next', 'not', 'only', 'other', 
+                 'have', 'had', 'just', 'make', 'next', 'not', 'only', 'other', 
                  'recommend', 'said', 'someone', 'then', 'told', 'very', 'was', 'were'])
+KEY_WORD = set(['burger', 'hamburger', 'burgers', 'hamburgers'])
+
 YELP_LKP_FILE ='Documents/Thinkful Project/thinkful course/capstone/yelp_lookup.py'
 YELP_BIZ_FILE = 'Canopy/Data/yelp_challenge/business.json'
 REVIEW_SUM_FILE = 'Documents/Thinkful Project/yelp_review.csv'
+REVIEW_TEXT_FILE = 'Documents/Thinkful Project/yelp_text.txt'
 
+review = pandas.read_csv(REVIEW_SUM_FILE)
 lookup = imp.load_source('', YELP_LKP_FILE)
 business_info = lookup.business_dict(YELP_BIZ_FILE)
-review = pandas.read_csv(REVIEW_SUM_FILE)
+sentiment_dict, num_positive, num_negative = lookup.sentiment_dict(REVIEW_TEXT_FILE, review)
 
-n, num_positive, num_negative = 0, 0, 0
-sentiment_dict = defaultdict(lambda: [0,0])
 common_words, bias_words = {}, {}
-# Do some word counts as next-step filters
-with open('Documents/Thinkful Project/yelp_text.txt', 'rb') as text: 
-    for line in text: 
-        if n % 50000 == 0: print n
-        line_clean = re.sub('\.{2,}', ', ', line.decode('utf8').rstrip('\n'))
-        blob_review = TextBlob(line_clean).lower()
-        word_set = set(blob_review.tags)
-        star = review['rating'][n]
-        if star >= 4: 
-            num_positive += 1
-            for word in word_set: 
-                if word[1] not in DROP_TAG: sentiment_dict[word][0] += 1
-        elif star <= 2: 
-            num_negative += 1
-            for word in word_set: 
-                if word[1] not in DROP_TAG: sentiment_dict[word][1] += 1
-        n += 1
 for k, v in sentiment_dict.iteritems(): 
     pos_freq, neg_freq = v[0]/num_positive, v[1]/num_negative
     if pos_freq > 0.25 and neg_freq > 0.25 and abs(math.log(pos_freq/neg_freq)) < 0.1: 
@@ -48,28 +33,37 @@ for k, v in sentiment_dict.iteritems():
     if pos_freq > 0.005 and neg_freq > 0.005 and abs(math.log(pos_freq/neg_freq)) > 1: 
         bias_words[k] = (pos_freq, neg_freq)
 
-# 2nd Run - Use the filters to generate positive/negative word baskets
-n = 0
-with open('Documents/Thinkful Project/yelp_text.txt', 'rb') as text: 
-    with open('Documents/Thinkful Project/all_review.basket', 'w') as output: 
-        for line in text: 
-            if n % 50000 == 0: print n
-            star = review['rating'][n]
-            labels = business_info[review['business_id'][n]][1]
-            if star == 3: 
+def word_basket(review, business_info, key_word): 
+    basket_file = ''
+    if len(key_word) == 0: basket_file = 'Documents/Thinkful Project/all_review.basket'
+    else: basket_file = 'Documents/Thinkful Project/keyword_review.basket'
+    n = 0
+    with open(REVIEW_TEXT_FILE, 'rb') as text: 
+        with open(basket_file, 'w') as output: 
+            for line in text: 
+                star = review['rating'][n]
+                labels = business_info[review['business_id'][n]][1]
+                line_clean = re.sub('\.{2,}', ', ', line.decode('utf8').rstrip('\n')).replace('=', ' ')
+                blob_review = TextBlob(line_clean)
+                word_set = set(blob_review.tags) - set(common_words.keys())
+                word_list = []
+                for word in word_set: 
+                    condition_1 = word[1] in DROP_TAG or word[0].lower() in DROP_WORD
+                    condition_2 = 'http' in word[0] or len(word[0]) <= 2
+                    if condition_1 or condition_2: continue
+                    word_list.extend(word[0].lower().split(','))
+                condition = True
+                if len(key_word) == 0: 
+                    condition = len(set(word_list)) == 0
+                    word_set = set(word_list) | set(labels)
+                else: 
+                    condition = len(set(word_list) & key_word) == 0
+                    word_set = set(word_list) - key_word
+                if condition or star == 3: 
+                    n += 1
+                    continue
+                word_string = ','.join(word_set)
+                if star >= 4: word_string += ',+'
+                elif star <= 2: word_string += ',-'
+                output.write(word_string.encode('utf8') + '\n')
                 n += 1
-                continue
-            line_clean = re.sub('\.{2,}', ', ', line.decode('utf8').rstrip('\n')).replace('=', ' ')
-            blob_review = TextBlob(line_clean)
-            word_set = set(blob_review.tags) 
-            word_set -= set(common_words.keys())
-            word_list = []
-            for word in word_set: 
-                if word[1] in DROP_TAG or 'http' in word[0] \
-                or word[0].lower() in DROP_WORD or len(word[0]) <= 2: continue
-                word_list.append(word[0].lower())
-            word_string = ','.join(set(word_list)|set(labels))
-            if star >= 4:  word_string += ',+'
-            elif star <= 2: word_string += ',-'
-            output.write(word_string.encode('utf8') + '\n')
-            n += 1
--- capstone/yelp_lookup.py
+++ capstone/yelp_lookup.py
@@ -1,5 +1,12 @@
+import re
 import json
+from textblob import TextBlob
+from collections import defaultdict 
 
+DROP_TAG = set(['CC', 'CD', 'DT', 'EX', 'IN', 'MD', 'NNP', 'NNPS', 
+                'POS', 'PRP', 'PRP$', 'SYM', 'TO', 'UH', 'WDT', 'WP', 'WRB'])
+
+# Find relevant info about business: state and category
 def business_dict(file_path): 
     business_lookup = {}
     with open(file_path) as data_file: 
@@ -13,3 +20,25 @@ def business_dict(file_path):
                 break
             business_lookup[business_id] = (state, category)
     return business_lookup
+
+# Count word frequencies by sentiment
+def sentiment_dict(file_path, review): 
+    n, num_positive, num_negative = 0, 0, 0
+    sentiment_dict = defaultdict(lambda: [0,0])
+    with open(file_path, 'rb') as text_file: 
+        for line in text_file: 
+            line_clean = re.sub('\.{2,}', ', ', line.decode('utf8').rstrip('\n'))
+            blob_review = TextBlob(line_clean).lower()
+            word_set = set(blob_review.tags)
+            star = review['rating'][n]
+            if star >= 4: 
+                num_positive += 1
+                for word in word_set: 
+                    if word[1] not in DROP_TAG: sentiment_dict[word][0] += 1
+            elif star <= 2: 
+                num_negative += 1
+                for word in word_set: 
+                    if word[1] not in DROP_TAG: sentiment_dict[word][1] += 1
+            n += 1
+    print "{0} positive reviews and {1} negative reviews".format(num_positive, num_negative)
+    return sentiment_dict, num_positive, num_negative
